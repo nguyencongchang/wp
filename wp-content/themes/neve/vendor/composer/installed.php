781df34f0722bcf807270f9d9ca17ac069cd5d43
HongDoan <doanhongg199@gamil.com>
2023-03-19 19:13:52 +0700

    create new theme

---
+++ wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/neve',
+        'pretty_version' => 'v3.5.5',
+        'version' => '3.5.5.0',
+        'reference' => 'b5d21024a4344ec6b08c1a16d719c23c332f809b',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/neve' => array(
+            'pretty_version' => 'v3.5.5',
+            'version' => '3.5.5.0',
+            'reference' => 'b5d21024a4344ec6b08c1a16d719c23c332f809b',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.2.37',
+            'version' => '3.2.37.0',
+            'reference' => 'a2079ee8cea68b5dbaabec1e06f15048ae9c9de7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wptt/webfont-loader' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0294f21a20549f0c5e79700399946583dfa496b0',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../wptt/webfont-loader',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
