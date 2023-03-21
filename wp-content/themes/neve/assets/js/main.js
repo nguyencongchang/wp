ae3b5a8d65414afcf9e34acd25910423c94e7cdc
Chang Nguyen <chang@canary-software.com>
2023-03-21 23:10:35 +0700

    update footer

--- wp-content/themes/neve/assets/js/main.js
+++ wp-content/themes/neve/assets/js/main.js
@@ -8,4 +8,22 @@ jQuery(document).ready(function($) {
         step: 1,
         delay: 100
     });
+
+    function reveal() {
+        let reveals = document.querySelectorAll(".reveal");
+
+        for (let i = 0; i < reveals.length; i++) {
+            let windowHeight = window.innerHeight;
+            let elementTop = reveals[i].getBoundingClientRect().top;
+            let elementVisible = 150;
+
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        }
+    }
+
+    window.addEventListener("scroll", reveal);
 });
--- wp-content/themes/neve/footer.php
+++ wp-content/themes/neve/footer.php
@@ -13,15 +13,57 @@
  *
  * @since 1.0.4
  */
-do_action( 'neve_before_primary_end' ); ?>
+do_action('neve_before_primary_end'); ?>
 
 </main><!--/.neve-main-->
 
 <footer class="bg-footer">
     <?php
-        $title = get_field('title', 'options');
+    $title = get_field('title', 'options');
     ?>
-    <h2><?php echo $title;?></h2>
+    <div class="container reveal">
+        <div class="row align-items-center">
+            <div class="col-lg-9">
+                <h4 class="name wow fadeInUp"
+                    style="visibility: visible; animation-name: fadeInUp;"><?php echo $title; ?></h4>
+                <div class="row">
+                    <?php if (have_rows('information', 'options')) : ?>
+                        <?php while (have_rows('information', 'options')): ?>
+                            <?php the_row();
+                            $office  = get_sub_field('office');
+                            $address = get_sub_field('address');
+                            $phone   = get_sub_field('phone');
+                            $fax     = get_sub_field('fax');
+                            $email   = get_sub_field('email');
+
+                            ?>
+                            <div class="col-lg-7 col-md-6 wow fadeInUp"
+                                 style="visibility: visible; animation-name: fadeInUp;">
+                                <div class="f-line"><i class="fa fa-home" aria-hidden="true"></i><span class="ml-2"><?php echo $office; ?></span> : <br>
+                                    </div>
+                                <div class="f-line">
+                                    <span class="ml-2"><?php echo $address; ?></span>
+                                </div>
+                                <div class="f-line"><i class="fa fa-phone" aria-hidden="true"></i><span class="ml-2"><?php echo $phone; ?></span></div>
+                                <div class="f-line"><i class="fa fa-fax" aria-hidden="true"></i><span class="ml-2"><?php echo $fax; ?></span></div>
+                                <div class="f-line"><i class="fa fa-envelope" aria-hidden="true"></i><span class="ml-2"><?php echo $email; ?></span> |&nbsp;
+                                </div>
+                            </div>
+
+                        <?php endwhile; ?>
+                    <?php endif; ?>
+                    <div class="col-lg-3 order-lg-last text-center wow fadeInUp"
+                         style="visibility: visible; animation-name: fadeInUp;">
+                        <p><img class="header-logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/header/newlogo-edit-02.png" alt="new-logo" />
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
 </footer>
 <?php
 
@@ -30,38 +72,39 @@ do_action( 'neve_before_primary_end' ); ?>
  *
  * @since 1.0.4
  */
-do_action( 'neve_after_primary' );
+do_action('neve_after_primary');
 
 /**
  * Filters the content parts.
  *
+ * @param bool $status Whether the component should be displayed or not.
+ * @param string $context The context name.
+ *
  * @since 1.0.9
  *
- * @param bool   $status Whether the component should be displayed or not.
- * @param string $context The context name.
  */
-if ( apply_filters( 'neve_filter_toggle_content_parts', true, 'footer' ) === true ) {
+if (apply_filters('neve_filter_toggle_content_parts', true, 'footer') === true) {
 
-	/**
-	 * Executes actions before the footer was rendered.
-	 *
-	 * @since 1.0.0
-	 */
-	do_action( 'neve_before_footer_hook' );
+    /**
+     * Executes actions before the footer was rendered.
+     *
+     * @since 1.0.0
+     */
+    do_action('neve_before_footer_hook');
 
-	/**
-	 * Executes the rendering function for the footer.
-	 *
-	 * @since 1.0.0
-	 */
-	do_action( 'neve_do_footer' );
+    /**
+     * Executes the rendering function for the footer.
+     *
+     * @since 1.0.0
+     */
+    do_action('neve_do_footer');
 
-	/**
-	 * Executes actions after the footer was rendered.
-	 *
-	 * @since 1.0.0
-	 */
-	do_action( 'neve_after_footer_hook' );
+    /**
+     * Executes actions after the footer was rendered.
+     *
+     * @since 1.0.0
+     */
+    do_action('neve_after_footer_hook');
 }
 ?>
 
@@ -75,7 +118,7 @@ wp_footer();
  *
  * @since 2.11
  */
-do_action( 'neve_body_end_before' );
+do_action('neve_body_end_before');
 
 ?>
 </body>
